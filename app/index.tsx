70009eec4e969b5814f955b4c0ed972e6602386e
GabrHenr <gabrielhenriquedxd@gmail.com>
2026-03-27 18:26:00 -0300

    Creating First screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,59 @@
-import { Text, View } from "react-native";
+import { Background, Button } from "@react-navigation/elements";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#b7e0d3",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  });
+  const handleLogin = () => {
+    // Implement your login logic here
+    alert(`Username: ${username}, Password: ${password}`);
+  };
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Climate and Weather App</Text>
+      <Text>Type your User</Text>
+      <TextInput
+        placeholder="Enter your username"
+        style={{
+          borderWidth: 1,
+          borderColor: "gray",
+          width: 200,
+          height: 40,
+          marginTop: 10,
+        }}
+        value={username}
+        onChangeText={(input) => setUsername(input)}
+      />
+      <Text>Type your Password</Text>
+      <TextInput
+        placeholder="Enter your password"
+        secureTextEntry
+        style={{
+          borderWidth: 1,
+          borderColor: "gray",
+          width: 200,
+          height: 40,
+          marginTop: 10,
+        }}
+        value={password}
+        onChangeText={(input) => setPassword(input)}
+      />
+      <StatusBar style="auto" />
+      <Button id="login-button" onPress={handleLogin}>
+        Login
+      </Button>
     </View>
   );
 }
